49e49b0ff7dd5938f6999bc4f4621b3cf82f8c71
RedPandoraya <kudobbs45@gmail.com>
2025-09-28 07:45:28 +0700

    balik

--- Belajar Terbuka/main.py
+++ Belajar Terbuka/main.py
@@ -51,21 +51,3 @@ print("angka yang dimasukkan adalah= ", data, type(data))
 data=bool(input("masukkan data= "))
 print("data yang dimasukkan adalah= ", data, type(data))
 #selain angka 0, maka hasilnya akan True
-
-
-#episode 8 operasi matematika
-#penjumlahan (+)
-#pengurangan (-)
-#perkalian (*)
-#pembagian (/)
-#pangkat (**)
-#modulus/sisa bagi (%)
-#pembagian bulat (//)
-print(10+3) #penjumlahan
-print(10-3) #pengurangan
-print(10*3) #perkalian
-print(10/3) #pembagian
-print(10**3) #pangkat
-print(10%3) #modulus/sisa bagi
-print(10//3) #pembagian bulat
-#pembagian bulat akan dibulatkan ke bawah
