c67f3daec05000b20f350baf32fc4a5f5003c257
RedPandoraya <kudobbs45@gmail.com>
2025-10-01 17:58:13 +0700

    penambahan beberapa materi

--- Belajar Terbuka/episode17.py
+++ Belajar Terbuka/episode17.py
@@ -37,4 +37,41 @@ print('Apakah', j ,'Bisa disebut sebagai judul= ', str(cek))
 j="Don't Die"
 cek= j.istitle()
 print('Apakah', j ,'Bisa disebut sebagai judul= ', str(cek))
-# Hasilnya akan False, karena Don't Die bukanlah penulisan judul yang benar karena ada tanda ('), sehingga tidak dihitung sebagai judul
+#Hasilnya akan False, karena Don't Die bukanlah penulisan judul yang benar karena ada tanda ('), sehingga tidak dihitung sebagai judul
+
+# Mengecek komponen startswith(), dan endswith()
+start = "Annyeong haseyo".startswith('Annyeong')
+print('dimulai dari Annyeong= ', str(start)) #True
+end = "Annyeong haseyo".endswith('Haseyo')
+print('diakhiri dengan Haseyo= ', str(end)) #hasilnya akan Falase karena "H dari "Haseyo" itu huruf kapital
+
+# Tanda [] arinya list adalah untuk meletakkan kumpulan data
+# Penggabungan komponen pengabungan/join(), dan pemisahan/split()
+
+# JOIN
+a ='aku','anak','sehat'
+join = ' '.join(a) #dalam tanda kutip jika tidak diberi space maka kata-katanya akan menempel
+print(join)
+
+p= 'aku,anak,sehat,tubuhku,kuat'
+print(p.split(','))
+
+# Alokasi karakter rjust(),     ljust(),  center()
+#                  rata kanan, rata kiri, tengah
+
+r= "rata kanan".rjust(20) #rata kanan ada 10 kata, jika rata kanan maka bagian kiri masih ada 10 "space"
+print(".",r,".")
+l= "rata kiri".ljust(20)
+print(".",l,".")
+c= "tengah".center(20)
+print(".",c,".")
+
+# Kebalikannya disebut sebagai strip()
+c= c.strip(" ") # isi tanda "" sesuai dengan apa yg kosong sebelumnya
+print(".",c,".")
+
+    # MISALNYA
+c= "tengah".center(20,"'") # ada pemisah setiap kata dengan tanda single quote (') 
+print(".",c,".")
+c= c.strip("'") # isi tanda "" sesuai dengan apa yg kosong sebelumnya, yaitu ('), maka tanda (') akan hilang
+print(".",c,".")
