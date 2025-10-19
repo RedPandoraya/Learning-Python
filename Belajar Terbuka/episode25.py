d73cb2861a631343b79ea3f4cef18577d4c39fb0
RedPandoraya <kudobbs45@gmail.com>
2025-10-19 18:53:30 +0700

    While Loop (Perulangan)

---
+++ Belajar Terbuka/episode25.py
@@ -0,0 +1,20 @@
+# Episode 25 While Loop (Perulangan)
+
+# While loop adalah dengan kondisi tertentu, selama kondisi itu terpenuhi maka perulangan akan terus dilakukan
+# while kondisi: dengan boolean
+# Cara eksekusi while:
+# While Kondisi :
+    # aksi sekarang
+    # aksi nanti
+
+#a= 10
+#while a>5:
+    #print("hello")
+# jika programnya dijalankan maka akan terus karena akan terus dianggap true (a>)
+
+a= 0
+while a<5:
+    a+=1 #setiap loop akan ditambah 1
+    print("hello")
+# Note : pastikan kondisi pada while akan bernilai false pada suatu saat agar tidak terjadi infinite loop (perulangan tanpa henti)
+
